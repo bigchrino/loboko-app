72269229881df32c8c6aee49364f7af11e32c913
Big Chrino <chrinovicmukengeshayi@gmail.com>
2026-05-27 16:01:18 +0800

    Update MyOrders.tsx

--- app/frontend/src/pages/MyOrders.tsx
+++ app/frontend/src/pages/MyOrders.tsx
@@ -41,6 +41,7 @@ export default function MyOrders() {
   
   const paymentFr: Record<string, string> = {
     pending: 'En attente',
+    held: 'En sécurité',
     paid: 'Payé',
     failed: 'Échoué',
     refunded: 'Remboursé',
